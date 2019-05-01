f4e987937f7907524c9028bb2d581c9a78616045
Anubhav <gupta.anubhav25@gmail.com>
2019-05-01 15:07:30 +0530

    =validation fix

--- routes/admin.js
+++ routes/admin.js
@@ -1,98 +1,96 @@
-var express = require('express');
-var User = require('../models/user');
-var UserRequest = require('../models/user_request');
-var Login = require('../models/login');
-var sendMail = require('./modules/email');
-var passport = require('passport');
+var express = require("express");
+var User = require("../models/user");
+var UserRequest = require("../models/user_request");
+var Login = require("../models/login");
+var sendMail = require("./modules/email");
+var passport = require("passport");
 
 var app = express.Router();
-app.get('/isLoggedIn', (req, res) => {
-
-    if (req.isAuthenticated()) {
-        User.findById(req.user.userid, (err, user) => {
-
-            if (err) {
-                res.json({ message: false })
-            } else {
-                console.log(user)
-                if (user.admin) {
-                    console.log(user)
-                    res.json({ message: true, user: user });
-                } else {
-                    res.json({ message: false })
-                }
-
-            }
-        });
-
-    } else {
-        res.json({ message: false })
-    }
-
+app.get("/isLoggedIn", (req, res) => {
+  if (req.isAuthenticated()) {
+    User.findById(req.user.userid, (err, user) => {
+      if (err) {
+        res.json({ message: false });
+      } else {
+        console.log(user);
+        if (user.admin) {
+          console.log(user);
+          res.json({ message: true, user: user });
+        } else {
+          res.json({ message: false });
+        }
+      }
+    });
+  } else {
+    res.json({ message: false });
+  }
 });
-app.get('/userRequests', (req, res) => {
-    UserRequest.find({})
-        .limit(20)
-        .exec((err, data) => {
-            if (err) {
-                res.json([]);
-            } else {
-                res.json(data);
-            }
-        });
-
-
-})
-
-
+app.get("/userRequests", (req, res) => {
+  UserRequest.find({})
+    .limit(20)
+    .exec((err, data) => {
+      if (err) {
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    });
+});
 
 function gernatePassword() {
-    let pwd = '';
-    let a = 'abcdefghijklmnopqrstuvwxyz1234657890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    for (let i = 0; i < 6; i++) {
-        pwd += a.charAt(Math.random() * a.length);
-    }
-    return pwd;
+  let pwd = "";
+  let a = "abcdefghijklmnopqrstuvwxyz1234657890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = 0; i < 6; i++) {
+    pwd += a.charAt(Math.random() * a.length);
+  }
+  return pwd;
 }
 
-
-app.post('/acceptUser', (req, res) => {
-    UserRequest.findOne({ 'username': req.body.username }, (err, data) => {
+app.post("/acceptUser", (req, res) => {
+  UserRequest.findOne({ username: req.body.username }, (err, data) => {
+    if (err) {
+      res.json({ response: "fail" });
+    } else {
+      var tmpUser = {
+        username: data.username,
+        email: data.email,
+        phoneNo: data.phoneNo,
+        fullname: data.fullname,
+        dob: data.dob,
+        gender: data.gender,
+        imglink: data.imglink,
+        thumbnail: data.thumbnail
+      };
+      if (data.description) {
+        tmpUser.description = data.description;
+      }
+      var myuser = new User(tmpUser);
+      myuser.save((err, dbuser) => {
         if (err) {
-            res.json({ response: 'fail' });
+          console.log(err);
+          res.json({ response: "fail" });
         } else {
-
-            var tmpUser = {
-                username: data.username,
-                email: data.email,
-                phoneNo: data.phoneNo,
-                fullname: data.fullname,
-                dob: data.dob,
-                gender: data.gender,
-                imglink: data.imglink,
-                thumbnail: data.thumbnail
-            }
-            if (data.description) {
-                tmpUser.description = data.description;
-            }
-            var myuser = new User(tmpUser);
-            myuser.save((err, dbuser) => {
-                if (err) {
+          UserRequest.remove({ username: dbuser.username }, err => {
+            if (err) {
+              res.json({ response: "fail" });
+            } else {
+              res.json({ response: "success" });
+              var pwd = gernatePassword();
+              Login.register(
+                new Login({
+                  username: dbuser.username,
+                  email: dbuser.email,
+                  userid: dbuser._id
+                }),
+                pwd,
+                (err, loginuser) => {
+                  if (err) {
                     console.log(err);
-                    res.json({ response: 'fail' })
-                } else {
-                    UserRequest.remove({ username: dbuser.username }, (err) => {
-                        if (err) {
-                            res.json({ response: 'fail' })
-                        } else {
-                            res.json({ response: 'success' });
-                            var pwd = gernatePassword();
-                            Login.register(new Login({ username: dbuser.username, email: dbuser.email, userid: dbuser._id }), pwd, (err, loginuser) => {
-                                if (err) {
-                                    console.log(err);
-                                    res.json({ response: 'fail' });
-                                } else {
-                                    sendMail('registration successful', `<html><body>
+                    res.json({ response: "fail" });
+                  } else {
+                    sendMail(
+                      "registration successful",
+                      `<html><body>
                                     <h4>Congratulations!, Your Socializer account was accepted by the admin.</h4>
                                     <h4>Your login details are : </h4>
                                     <h3>Username :  ${loginuser.username}</h3>
@@ -100,30 +98,45 @@ app.post('/acceptUser', (req, res) => {
                                     <h4><a href='https://mysocializer.herokuapp.com/login'>Click here to login</a></h4>
                                     <h4><a href='https://mysocializer.herokuapp.com/changepassword'>Click here to change password</a></h4>
 
-                                   </body> </html>`, loginuser.email)
-                                    passport.authenticate('local')(req, res, () => {
-                                        res.json({ response: 'success' });
-                                    })
-                                }
-                            })
-                        }
-                    })
-
+                                   </body> </html>`,
+                      loginuser.email
+                    );
+                    console.log(pwd);
+                    passport.authenticate("local")(req, res, () => {
+                      res.json({ response: "success" });
+                    });
+                  }
                 }
-            })
+              );
+            }
+          });
         }
-    })
-})
+      });
+    }
+  });
+});
 
-
-app.post('/rejectUser', (req, res) => {
-    UserRequest.remove({ username: req.body.username }, (err) => {
-        if (err) {
-            res.json({ response: 'fail' })
-        } else {
-            res.json({ response: 'success' });
-        }
-    })
-})
+app.post("/rejectUser", (req, res) => {
+  UserRequest.remove({ username: req.body.username }, err => {
+    if (err) {
+      res.json({ response: "fail" });
+    } else {
+      res.json({ response: "success" });
+    }
+  });
+});
+// Login.register(
+//   new Login({
+//     username: "anubhav",
+//     email: "anubhav@gmail.com",
+//     userid: "5a9131ff1f7f4f0014b936d8"
+//   }),
+//   "anubhav",
+//   (err, loginuser) => {
+//     Login.find({}, (e, u) => {
+//       console.log(u);
+//     });
+//   }
+// );
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var User = require('../models/user');
-var UserRequest = require('../models/user_request');
-var Login = require('../models/login');
-var passport = require('passport');
-var image = require('./modules/image')
-var upload = require('./modules/upload')
-var thumb = require('node-thumbnail').thumb;
+var express = require("express");
+var User = require("../models/user");
+var UserRequest = require("../models/user_request");
+var Login = require("../models/login");
+var passport = require("passport");
+var image = require("./modules/image");
+var upload = require("./modules/upload");
+var thumb = require("node-thumbnail").thumb;
 
 var app = express.Router();
 
@@ -18,182 +18,242 @@ var app = express.Router();
 }
  */
 
-app.get('/isLoggedIn', (req, res) => {
-    if (req.isAuthenticated()) {
-        User.findById(req.user.userid, (err, user) => {
-            if (err) {
-                res.json({ message: false })
-            } else {
-                res.json({ message: true, user: user });
-            }
-        })
+app.get("/isLoggedIn", (req, res) => {
+  if (req.isAuthenticated()) {
+    User.findById(req.user.userid, (err, user) => {
+      if (err) {
+        res.json({ message: false });
+      } else {
+        res.json({ message: true, user: user });
+      }
+    });
+  } else {
+    res.json({ message: false });
+  }
+});
 
+app.post("/usernameValid", (req, res) => {
+  //check of if the username is already available
+  //if already exists send false
+  //else send true
+  User.find({ username: req.body.username }, (err, data) => {
+    if (err) {
+      console.log(err);
     } else {
-        res.json({ message: false })
-    }
-
-});
-
-
-app.post('/usernameValid', (req, res) => {
-    //check of if the username is already available
-    //if already exists send false
-    //else send true
-    if (req.body.username == 'anubhav')
+      if (data && data.length > 0) {
         res.json({ response: false });
-    else
-        res.json({ response: true });
-});
-app.post('/emailValid', (req, res) => {
-    //check of if the email is already available
-    //if already exists send false
-    //else send true
-    if (req.body.email == 'gupta@mailinator.com')
-        res.json({ response: false });
-    else
-        res.json({ response: true });
-});
-
-app.post('/phoneNoValid', (req, res) => {
-    //check of if the email is already available
-    //if already exists send false
-    //else send true
-    if (req.body.phoneNo == '7404541565')
-        res.json({ response: false });
-    else
-        res.json({ response: true });
-});
-
-
-
-app.post('/changePassword', (req, res) => {
-
-    Login.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-            res.json({ response: 'fail', message: err.message })
-        } else if (!user) {
-            res.json({ response: 'fail', message: 'username does not exists' })
-        } else {
-
-            user.changePassword(req.body.oldpassword, req.body.newpassword, (err, newuser) => {
-                if (err) {
-                    res.json({ response: 'fail', message: err.message })
-                } else {
-                    res.json({ response: 'success' })
-                }
-            })
-        }
-    })
-})
-
-
-app.post('/register', (req, res) => {
-
-    var nameOfFile = 'anubhav-' + Date.now();
-    upload('file', nameOfFile, 'profilePictures', req, res, function(err) {
-        if (err) {
-            res.json({ response: 'fail' });
+      } else {
+        UserRequest.find({ username: req.body.username }, (err, data1) => {
+          if (err) {
             console.log(err);
-        } else {
-
-
-            if (req.file) {
-                if (parseInt(req.body.rotation) > 0) {
-                    image.rotate(req.file.path, req.body.rotation);
-                } else {
-                    image.compress(req.file.path);
-                }
-                console.log(req.file)
-                thumb({
-                    source: req.file.path,
-                    destination: 'C:\\Users\\ANUBHAV\\Desktop\\New folder\\Angular\\Socializer\\src\\assets\\thumbnails\\',
-                    concurrency: 1,
-                    basename: nameOfFile,
-                    suffix: '',
-                    width: 300
-                }, function(files, err, stdout, stderr) {
-                    console.log(files)
-                });
-                delete req.body.rotation;
+          } else {
+            if (data1 && data1.length > 0) {
+              res.json({ response: false });
             } else {
-                req.body.imglink = '/assets/profilePictures/default.png'
-
+              res.json({ response: true });
             }
-            req.body.thumbnail = req.body.imglink.replace('profilePictures', 'thumbnails')
-            let user = new UserRequest(req.body);
-            user.save((err, user) => {
-                if (err) {
-                    console.log(err);
-                    res.json({ response: 'fail' })
-                } else {
-                    res.json({ response: 'success' });
-                }
-            })
-        }
-
-    })
-})
-
-app.get('/search/:query', (req, res) => {
-    var myquery = req.params.query;
-    myquery = '.*' + myquery + '.*';
-    var regex = new RegExp(myquery, 'i');
-    console.log(regex)
-    var query = User.find({ username: regex }, { 'username': 1 }).limit(20);
-    query.exec(function(err, users) {
-        if (!err) {
-            var query = UserRequest.find({ username: regex }, { 'username': 1, 'thumbnail': 1 }).limit(5);
-            query.exec(function(err, users2) {
-                if (!err) {
-                    res.json(users.concat(users2));
-                } else {
-                    res.json(err);
-                }
-            });
-        } else {
-            res.json(err);
-        }
-    });
-})
-
-app.get('/user/:username', (req, res) => {
-
-    var query = User.findOne({ username: req.params.username }, (err, user) => {
-        if (err) {
-            res.json(err);
-        } else {
-            if (!user) {
-                var query = UserRequest.findOne({ username: req.params.username }, (err, users2) => {
-                    if (!err) {
-                        res.json(users2);
-                    } else {
-                        res.json(err);
-                    }
-                });
-            } else {
-                res.json(user);
-            }
-        }
-    });
-})
-
-
-app.post('/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) { return next(err); }
-        if (!user) { return res.json({ response: 'fail', message: info.message }) }
-        req.logIn(user, function(err) {
-            if (err) { return next(err); }
-            return res.json({ response: 'success', path: '/home', user: { username: user.username, _id: user._id } });
+          }
         });
-    })(req, res, next);
+      }
+    }
+  });
+});
+app.post("/emailValid", (req, res) => {
+  //check of if the email is already available
+  //if already exists send false
+  //else send true
+  User.find({ email: req.body.email }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (data && data.length > 0) {
+        res.json({ response: false });
+      } else {
+        UserRequest.find({ email: req.body.email }, (err, data1) => {
+          if (err) {
+            console.log(err);
+          } else {
+            if (data1 && data1.length > 0) {
+              res.json({ response: false });
+            } else {
+              res.json({ response: true });
+            }
+          }
+        });
+      }
+    }
+  });
 });
 
+app.post("/phoneNoValid", (req, res) => {
+  //check of if the email is already available
+  //if already exists send false
+  //else send true
+  User.find({ phoneNo: req.body.phoneNo }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (data && data.length > 0) {
+        res.json({ response: false });
+      } else {
+        UserRequest.find({ phoneNo: req.body.phoneNo }, (err, data1) => {
+          if (err) {
+            console.log(err);
+          } else {
+            if (data1 && data1.length > 0) {
+              res.json({ response: false });
+            } else {
+              res.json({ response: true });
+            }
+          }
+        });
+      }
+    }
+  });
+});
 
+app.post("/changePassword", (req, res) => {
+  Login.findOne({ username: req.body.username }, (err, user) => {
+    if (err) {
+      res.json({ response: "fail", message: err.message });
+    } else if (!user) {
+      res.json({ response: "fail", message: "username does not exists" });
+    } else {
+      user.changePassword(
+        req.body.oldpassword,
+        req.body.newpassword,
+        (err, newuser) => {
+          if (err) {
+            res.json({ response: "fail", message: err.message });
+          } else {
+            res.json({ response: "success" });
+          }
+        }
+      );
+    }
+  });
+});
 
-app.get('/logout', (req, res) => {
-    req.logout();
-})
+app.post("/register", (req, res) => {
+  var nameOfFile = "anubhav-" + Date.now();
+  upload("file", nameOfFile, "profilePictures", req, res, function(err) {
+    if (err) {
+      res.json({ response: "fail" });
+      console.log(err);
+    } else {
+      if (req.file) {
+        if (parseInt(req.body.rotation) > 0) {
+          image.rotate(req.file.path, req.body.rotation);
+        } else {
+          image.compress(req.file.path);
+        }
+        console.log(req.file);
+        thumb(
+          {
+            source: req.file.path,
+            destination:
+              "C:\\Users\\ANUBHAV\\Desktop\\New folder\\Angular\\Socializer\\src\\assets\\thumbnails\\",
+            concurrency: 1,
+            basename: nameOfFile,
+            suffix: "",
+            width: 300
+          },
+          function(files, err, stdout, stderr) {
+            console.log(files);
+          }
+        );
+        delete req.body.rotation;
+      } else {
+        req.body.imglink = "/assets/profilePictures/default.png";
+      }
+      req.body.thumbnail = req.body.imglink.replace(
+        "profilePictures",
+        "thumbnails"
+      );
+      let user = new UserRequest(req.body);
+      user.save((err, user) => {
+        if (err) {
+          console.log(err);
+          res.json({ response: "fail" });
+        } else {
+          res.json({ response: "success" });
+        }
+      });
+    }
+  });
+});
 
+app.get("/search/:query", (req, res) => {
+  var myquery = req.params.query;
+  myquery = ".*" + myquery + ".*";
+  var regex = new RegExp(myquery, "i");
+  console.log(regex);
+  var query = User.find({ username: regex }, { username: 1 }).limit(20);
+  query.exec(function(err, users) {
+    if (!err) {
+      var query = UserRequest.find(
+        { username: regex },
+        { username: 1, thumbnail: 1 }
+      ).limit(5);
+      query.exec(function(err, users2) {
+        if (!err) {
+          res.json(users.concat(users2));
+        } else {
+          res.json(err);
+        }
+      });
+    } else {
+      res.json(err);
+    }
+  });
+});
+
+app.get("/user/:username", (req, res) => {
+  var query = User.findOne({ username: req.params.username }, (err, user) => {
+    if (err) {
+      res.json(err);
+    } else {
+      if (!user) {
+        var query = UserRequest.findOne(
+          { username: req.params.username },
+          (err, users2) => {
+            if (!err) {
+              res.json(users2);
+            } else {
+              res.json(err);
+            }
+          }
+        );
+      } else {
+        res.json(user);
+      }
+    }
+  });
+});
+
+app.post("/login", function(req, res, next) {
+  passport.authenticate("local", function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.json({ response: "fail", message: info.message });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res.json({
+        response: "success",
+        path: "/home",
+        user: { username: user.username, _id: user._id }
+      });
+    });
+  })(req, res, next);
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+});
 
 module.exports = app;
